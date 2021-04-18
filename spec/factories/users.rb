b35c1520afc53a85032d900e4eb7eafa92fd877b
Hayashi-Satoki <stk.884@gmail.com>
2021-04-18 16:30:21 +0900

    修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
-    password_confirmation {password}
-    family_name           {'abe'}
-    first_name            {'ken'}
-    family_name_kana      {'アベ'}
-    first_name_kana       {'ケン'}
-    birthday              {'2020/01/01'}
+    nickname              { 'test' }
+    email                 { 'test@example' }
+    password              { '123abc' }
+    password_confirmation { password }
+    family_name           { '阿部' }
+    first_name            { '健' }
+    family_name_kana      { 'アベ' }
+    first_name_kana       { 'ケン' }
+    birthday              { '2020/01/01' }
   end
 end
