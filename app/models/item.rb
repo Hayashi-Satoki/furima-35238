25af76bae910223aaf37890791f1a78a19304de7
Hayashi-Satoki <stk.884@gmail.com>
2021-04-19 17:04:26 +0900

    ActiveHashとバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,28 @@
 class Item < ApplicationRecord
-  berongs_to :user
+  belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+  belongs_to :condition_id
+  belongs_to :shipping_fee_id
+  belongs_to :prefecture_id
+  belongs_to :required_day_id
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+  end
+
+  validates :price, presence: true, format:{ with: /^[0-9]+$/ } , numericality{ greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :required_day_id
+  end
+
 end
