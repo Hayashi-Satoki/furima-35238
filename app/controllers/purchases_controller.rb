6b88c923275c85db95eac3b44afe7bf25027ba0a
Hayashi-Satoki <stk.884@gmail.com>
2021-04-26 14:31:36 +0900

    決済機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,9 +7,11 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    binding.pry
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_address_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
@@ -20,7 +22,16 @@ class PurchasesController < ApplicationController
   private
 
    def purchase_address_params
-    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :street, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :street, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+   end
+
+   def pay_item
+    Payjp.api_key = "sk_test_cdc5250407593e18446fa1f3"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: purchase_address_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
    end
 
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,12 +1,13 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :street, :building, :phone, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :city, :street, :building, :phone, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :city
     validates :street
     validates :phone, format: {with: /\A\d{10,11}\z/ }
+    validates :token
   end
 
   validates :prefecture_id, numericality: { other_than: 1 }
